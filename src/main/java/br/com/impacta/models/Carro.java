89bd5d5cf3400fb0c04adaf20fd5d6394bf2ddb7
Thiago Pereira <thiago.psilveira@aluno.faculdadeimpacta.com.br>
2021-03-04 22:59:07 +0000

    Atividade Lab 07

---
+++ src/main/java/br/com/impacta/models/Carro.java
@@ -0,0 +1,37 @@
+package br.com.impacta.models;
+
+public class Carro {
+	
+	private Integer ano;
+	
+	private String cor;
+	
+	private Double valor;
+	
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
